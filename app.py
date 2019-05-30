eca1300d35aeaf9ad60e29dc97bab43b114551cb
jiangyun2 <570454176@qq.com>
2019-05-30 20:35:18 +0800

    添加端口定义

--- app.py
+++ app.py
@@ -3,6 +3,10 @@
 
 import tornado.ioloop
 import tornado.web
+from tornado.options import define,options
+
+# 定义端口信息
+define('port', default=8888, type=int, help="Listening port")
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -13,12 +17,19 @@ class MainHandler(tornado.web.RequestHandler):
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-    ])
+    ],
+        # debug模式下，检测到代码改变将自动重启tornado
+        debug=True,
+    )
 
 
 if __name__ == "__main__":
     app = make_app()
-    app.listen(8888)
+    # 命令行参数转换
+    tornado.options.parse_command_line()
+    # 控制台输出port
+    print("Server start on port {}".format(options.port))
+    app.listen(options.port)
     tornado.ioloop.IOLoop.current().start()
 
 
