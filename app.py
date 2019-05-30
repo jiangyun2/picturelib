5df8c077eef3b2465e6b14b1b10cc975965ecc71
jiangyun2 <570454176@qq.com>
2019-05-30 21:18:35 +0800

    代码重构

--- app.py
+++ app.py
@@ -4,17 +4,31 @@
 import tornado.ioloop
 import tornado.web
 from tornado.options import define, options
+from handlers import main
 import utils.ui_modules,utils.ui_methods
 
 # 定义端口信息
+from handlers.main import MainHandler
+
 define('port', default=8888, type=int, help="Listening port")
 
-
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render('home.html')
-
-
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", main.MainHandler),
+        ]
+        settings = dict(
+            # debug模式下，检测到代码改变将自动重启tornado
+            debug=True,
+            # 模板
+            template_path='templates',
+            # 静态文件
+            static_path='statics',
+            ui_modules=utils.ui_modules,
+            ui_methods=utils.ui_methods,
+        )
+        # 继承父类的init，主要起作用的是父类的init
+        super().__init__(handlers=handlers, **settings)
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
@@ -31,7 +45,7 @@ def make_app():
 
 
 if __name__ == "__main__":
-    app = make_app()
+    app = Application()
     # 命令行参数转换
     tornado.options.parse_command_line()
     # 控制台输出port
